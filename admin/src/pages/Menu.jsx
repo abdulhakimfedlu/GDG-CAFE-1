af23f9de7382ae7fc04e56670fe64041bda6f81f
Abdulhakim Fedlu <155722598+abdulhakimfedlu@users.noreply.github.com>
2025-06-14 22:32:58 +0300

    Update Menu.jsx

--- admin/src/pages/Menu.jsx
+++ admin/src/pages/Menu.jsx
@@ -3,6 +3,7 @@ import { Trash2, Edit, Plus, Save, X, AlertTriangle } from 'lucide-react';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
+
 // API base URL (adjust if needed)
 const API_BASE = '/api';
 
