f839711d78cc404a6be1e3ce9dc55bfc79a62556
Abdulhakim Fedlu <155722598+abdulhakimfedlu@users.noreply.github.com>
2025-06-14 22:31:54 +0300

    Update Employee.jsx

--- admin/src/pages/Employee.jsx
+++ admin/src/pages/Employee.jsx
@@ -3,6 +3,7 @@ import { Mail, Phone, User, Building, Calendar, DollarSign, Star, Search, Filter
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+
 const EmployeeListPage = () => {
   const [employees, setEmployees] = useState([]);
   const [loading, setLoading] = useState(true);
