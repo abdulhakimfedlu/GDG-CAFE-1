84ed8e42b806b0723fbe424f3b1f3d024e869372
Abdulhakim Fedlu <155722598+abdulhakimfedlu@users.noreply.github.com>
2025-06-29 22:49:43 +0300

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect, useRef } from 'react';
 import { Users, Phone, ArrowRight, Star, Heart, Zap, Sparkles, Sun, Droplets } from 'lucide-react';
 import { Link, useNavigate } from 'react-router-dom';
