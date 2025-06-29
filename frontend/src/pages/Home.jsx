b78eb55d13d12ddb1cdbe4109b2a1365bf311466
Abdulhakim Fedlu <155722598+abdulhakimfedlu@users.noreply.github.com>
2025-06-29 22:49:35 +0300

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useRef } from 'react';
-
 import { Users, Phone, ArrowRight, Star, Heart, Zap, Sparkles, Sun, Droplets } from 'lucide-react';
 import { Link, useNavigate } from 'react-router-dom';
 import LogoAnimation from '../components/LogoAnimation';
