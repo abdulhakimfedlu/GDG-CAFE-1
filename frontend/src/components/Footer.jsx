04541449be91a3f7eae73ca0792227ccf01a63ac
Abdulhakim Fedlu <155722598+abdulhakimfedlu@users.noreply.github.com>
2025-07-07 19:40:01 +0300

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Sparkles } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
 
-
 const Footer = () => {
   const navigate = useNavigate();
   return (
